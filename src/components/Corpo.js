d72eee8cfa4b1baa2d174b9974a377c4ce78c3a5
eduardowanderleyde <wanderley.eduardo@gmail.com>
2022-10-05 12:20:10 -0300

    “enviarfuncoes“

--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -2,15 +2,20 @@ import React from "react";
 import Dados from "./Dados";
 
 export default function Corpo() {
-  const cnl = "Wanderley ";
+  const cnl = () => {
+    return "Wanderley curso";
+  };
   const yt = "youtube.com/wanderleycursos";
   const crs = "React.js";
+  const somar = (v1, v2) => {
+    return v1 + v2;
+  };
   return (
     <section>
       <h2>Curso de React: </h2>
       <p>Se inscreva em nosso canal</p>
       <p>Ative o sininho e clique no sininho</p>
-      <Dados canal={cnl} youtube={yt} curso={crs} />
+      <Dados canal={cnl} youtube={yt} curso={crs} somar={somar} />
     </section>
   );
 }
--- src/components/Dados.js
+++ src/components/Dados.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 export default function Dados(props) {
+  const n1 = 10;
+  const n2 = 20;
   return (
     <section>
-      g<p>Canal:{props.canal}</p>
+      <p>Canal:{props.canal()}</p>
       <p>Youtube:{props.youtube}</p>
       <p>Curso: {props.curso}</p>
+      <p>
+        {"A soma de " + n1 + " com " + n2 + " é igual a " + props.somar(n1, n2)}
+      </p>
     </section>
   );
 }
